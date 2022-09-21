9f27706255dc22f2db12d567830c17e023a9d490
DHkimgit <dukekim001@naver.com>
2022-09-21 03:52:48 +0000

    09211252

---
+++ app/server/models/forum.py
@@ -0,0 +1,40 @@
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+class FourmSchema(BaseModel):
+    writer_name: str = Field(...)
+    writer_service_numer: str = Field(...)
+    category: str = Field(...)
+    title: str = Field(...)
+    content: str = Field(...)
+    write_date: str = Field(...)
+    write_time: str = Field(...)
+
+    class config():
+        schema_extra = {
+            "example": {
+                "writer_name": "Jhon doe",
+                "writer_service_numer": "22-2222222",
+                "category": "general",
+                "title": "test",
+                "content": "wow",
+                "write_date": "2022-09-21",
+                "write_time": "12:46:39",
+            }
+        }
+
+class CommentSchema(BaseModel):
+    username: str
+    servicenumber: str
+    content: str
+    comment_date: str
+
+    class config():
+        schema_extra = {
+            "example": {
+                "username": "Jhon doe",
+                "servicenumber": "22-2222222",
+                "content": "wow",
+                "comment_date": "2022-09-21"
+            }
+        }
